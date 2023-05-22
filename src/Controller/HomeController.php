d6a8da9d3594288c52caa30c890599cd58d919a8
vermin-le-germin <122983999+Antoine-developpeur@users.noreply.github.com>
2023-05-22 19:17:31 +0200

    finalisation de la structure de la page accueil-creation des tables articles et category

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,7 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
 }
+
+
---
+++ src/Entity/Article.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToMany(targetEntity: Category::class)]
+    private Collection $category;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Auteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Contenue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phraseAcroche = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $images = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tag = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titreImages = null;
+
+    public function __construct()
+    {
+        $this->category = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategory(): Collection
+    {
+        return $this->category;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->category->contains($category)) {
+            $this->category->add($category);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        $this->category->removeElement($category);
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): self
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+
+    public function getContenue(): ?string
+    {
+        return $this->Contenue;
+    }
+
+    public function setContenue(string $Contenue): self
+    {
+        $this->Contenue = $Contenue;
+
+        return $this;
+    }
+
+    public function getPhraseAcroche(): ?string
+    {
+        return $this->phraseAcroche;
+    }
+
+    public function setPhraseAcroche(string $phraseAcroche): self
+    {
+        $this->phraseAcroche = $phraseAcroche;
+
+        return $this;
+    }
+
+    public function getImages(): ?string
+    {
+        return $this->images;
+    }
+
+    public function setImages(string $images): self
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->tag;
+    }
+
+    public function setTag(string $tag): self
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    public function getTitreImages(): ?string
+    {
+        return $this->titreImages;
+    }
+
+    public function setTitreImages(string $titreImages): self
+    {
+        $this->titreImages = $titreImages;
+
+        return $this;
+    }
+}
